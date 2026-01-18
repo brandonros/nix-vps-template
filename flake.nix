cf0ca6ab9e6c8dd4d22a71bb4feb6335449e6471
Brandon Ros <brandonros1@gmail.com>
2026-01-18 07:20:59 -0500

    impors

--- flake.nix
+++ flake.nix
@@ -3,22 +3,34 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    impermanence.url = "github:nix-community/impermanence";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, disko, agenix, impermanence }:
     let
       forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
     in {
-    # Export modules for consumers
+    # Export individual modules for fine-grained control
     nixosModules = {
       base = ./nix/modules/platforms/base.nix;
       vultr = ./nix/modules/platforms/vultr.nix;
       hardening = ./nix/modules/security/hardening.nix;
     };
 
-    # Convenience bundle of all modules
+    # Convenience bundle with all dependencies included
     nixosModules.default = { ... }: {
       imports = [
+        disko.nixosModules.disko
+        agenix.nixosModules.default
+        impermanence.nixosModules.impermanence
         self.nixosModules.base
         self.nixosModules.vultr
         self.nixosModules.hardening
