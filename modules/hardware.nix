5166f8a681b0f0301c8602a603f1b24d10619502
Brandon Ros <brandonros1@gmail.com>
2026-01-19 13:15:01 -0500

    initrd hell

--- modules/hardware.nix
+++ modules/hardware.nix
@@ -8,8 +8,14 @@
     device = "nodev";
   };
 
-  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi" ];
-  boot.initrd.kernelModules = [ "nvme" ];
+  # Disk/storage drivers for various hypervisors - all force-loaded
+  boot.initrd.kernelModules = [
+    "virtio_pci" "virtio_blk" "virtio_scsi"   # KVM/Vultr
+    "ahci" "sd_mod" "nvme"                    # Generic
+    "xen_blkfront"                            # Xen
+    "vmw_pvscsi"                              # VMware
+    "ata_piix" "uhci_hcd"                     # Legacy
+  ];
   boot.tmp.cleanOnBoot = true;
 
   fileSystems."/" = {
